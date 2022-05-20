d6b8bf748f4cf3c6a214a3d34c8ed1ed7943af19
Ethan Lemke <ethan@mfbtech.com>
2022-05-20 21:03:10 +0000

    update publish command jsdoc, add todos

--- src/commands/publish.ts
+++ src/commands/publish.ts
@@ -1,10 +1,18 @@
 /**
  * Combines all of the existing changes files into the change log and compresses the version bumps
  * in the change files down to a single version, which is used to update the package.json version.
+ * 
+ * 
  */
 export function publish(args?: {
   /** Show this commands documentation and exit. */
   help?: boolean;
 }) {
+  /*
+    TODO:
+      1. check for changes files in the current project
+      1. get the current version from the package.json
+      TODO: add rest
+  */
   throw new Error("TODO");
 }
