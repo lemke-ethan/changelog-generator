c7250b34ddf9f6938d3687ed84a86886260ef932
Ethan Lemke <ethan@mfbtech.com>
2022-05-20 21:51:30 +0000

    add jsdoc comments to types and use new string constants

--- src/types/changeFile.ts
+++ src/types/changeFile.ts
@@ -1,25 +1,20 @@
+/** The different types of changes. */
 export enum ChangeType {
     MAJOR = "MAJOR",
     MINOR = "MINOR",
     PATCH = "PATCH",
     NONE = "NONE"
 }
-
 export type ChangeTypeStringLiteral = `${ChangeType}`
 
-export const ChangeTypeDescription: {[Property in ChangeTypeStringLiteral]: string} = {
-    [ChangeType.MAJOR]: "indicates that these changes incompatible API changes.",
-    [ChangeType.MINOR]: "indicates that these changes add functionality in a backwards compatible manner.",
-    [ChangeType.PATCH]: "indicates that these changes fix bugs in a backwards compatible manner.",
-    [ChangeType.NONE]: "indicates that these changes make changes to the source code that do not effect the user (e.g. change eslint rules, change a readme file, ...).",
-}
-
+/** Represents a change that can be recorded in a {@link ChangeFile}. */
 export type Change = {
     packageName: string
     comment: string
     type: ChangeType
 }
 
+/** Represents a change file. */
 export type ChangeFile = {
     changes: Change[]
 }
--- src/types/changeLog.ts
+++ src/types/changeLog.ts
@@ -1,15 +1,18 @@
-import { ChangeType, ChangeTypeStringLiteral } from "./changeFile"
+import { ChangeTypeStringLiteral } from "./changeFile"
 
+/** Represents a collection of comments in a change log entry. */
 export type ChangeLogEntryComments = {
     [Property in ChangeTypeStringLiteral]?: {comment: string}[]
 }
 
+/** Represents an entry in the change log. */
 export type ChangeLogEntry = {
     version: string,
     date: string,
     comments: ChangeLogEntryComments
 }
 
+/** Represents a change log. */
 export type ChangeLog = {
     name: string
     entries: ChangeLogEntry[]
