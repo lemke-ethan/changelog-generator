5af106ddb6bbc4b3de3f451d7e8c76de793db4d5
Ethan Lemke <ethan@mfbtech.com>
2022-06-03 21:35:53 +0000

    index: init the ccg command with the change subcommand

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
 import { change } from "./commands/change";
+import { Command } from "commander";
+import {
+  ChangeCommandDescription,
+  ChangeCommandName,
+  ChangeCommandOptionDescription,
+  ChangeCommandOptionFlag,
+} from "./strings/constants";
 
-change();
+const program = new Command("ccg");
+
+program
+  .command(ChangeCommandName)
+  .description(ChangeCommandDescription)
+  .option(ChangeCommandOptionFlag.verify, ChangeCommandOptionDescription.verify)
+  .action(change);
+
+program.parse(process.argv);
+
+console.log({
+  options: program.opts(),
+});
