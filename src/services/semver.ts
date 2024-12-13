43c1be954495647ba71dadffddeea73fcb102401
Ethan Lemke <ethan@mfbtech.com>
2024-12-13 16:24:04 +0000

    services/semver.ts: init semver with parseSemver function

---
+++ src/services/semver.ts
@@ -0,0 +1,21 @@
+// Copyright 2024 MFB Technologies, Inc.
+
+import { parse } from "semver"
+
+export type Semver = {
+  major: number
+  minor: number
+  patch: number
+}
+
+export function parseSemver(version: string): Semver | null {
+  const parsedSemver = parse(version)
+  if (parsedSemver === null) {
+    return null
+  }
+  return {
+    major: parsedSemver.major,
+    minor: parsedSemver.minor,
+    patch: parsedSemver.patch
+  }
+}
